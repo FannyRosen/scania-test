a77c6be8445cc45a91bbea4e76a5bb631ae22868
Fanny Rosén <fanny.rosen@umain.com>
2022-11-16 10:51:40 +0100

    edited dataset

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,20 +3,23 @@ export const MainContent = () => {
     const dropdown = document.querySelector('#dropdownExample');
     dropdown.resetOption();
   } */
+
+  function handleClick1() {}
+
   return (
     <>
       <div className="sdds-block sdds-block__onwhite">
         <sdds-dropdown placeholder="Select distance" id="dropdownExample">
-          <sdds-dropdown-option value="option-1">
+          <sdds-dropdown-option value="option-1" onClick={() => handleClick1()}>
             {"<=200.000 km"}
           </sdds-dropdown-option>
-          <sdds-dropdown-option value="option-2">
+          <sdds-dropdown-option value="option-2" onClick={() => handleClick1()}>
             {">200.000 km"}
           </sdds-dropdown-option>
         </sdds-dropdown>
-      </div>
-      <sdds-table
-        body-data='[{"driver":"Marcus Lundberg","company":"Aris FC","distance": 75044,"score": 52},
+
+        <sdds-table
+          body-data='[{"driver":"Marcus Lundberg","company":"Aris FC","distance": 75044,"score": 52},
   {
     "driver": "Marcus Mena Pachero",
     "company": "Lio LTD",
@@ -32,28 +35,29 @@ export const MainContent = () => {
   {
     "driver": "Niklas Rosén",
     "company": "DD Interactive",
-    "distance": "200 000",
+    "distance": 200000,
     "score": 78
   }
 ]'
-      >
-        <sdds-header-cell
-          column-key="driver"
-          column-title="Driver"
-        ></sdds-header-cell>
-        <sdds-header-cell
-          column-key="company"
-          column-title="Company"
-        ></sdds-header-cell>
-        <sdds-header-cell
-          column-key="distance"
-          column-title="Distance"
-        ></sdds-header-cell>
-        <sdds-header-cell
-          column-key="score"
-          column-title="Score"
-        ></sdds-header-cell>
-      </sdds-table>
+        >
+          <sdds-header-cell
+            column-key="driver"
+            column-title="Driver"
+          ></sdds-header-cell>
+          <sdds-header-cell
+            column-key="company"
+            column-title="Company"
+          ></sdds-header-cell>
+          <sdds-header-cell
+            column-key="distance"
+            column-title="Distance"
+          ></sdds-header-cell>
+          <sdds-header-cell
+            column-key="score"
+            column-title="Score"
+          ></sdds-header-cell>
+        </sdds-table>
+      </div>
 
       {/*   {DriverEvaluations.map((driver) => {
         return <div key={driver.id}></div>;
